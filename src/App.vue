d330be0690d4f0151f6e0395b76f12a62e8332f3
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-11-04 20:54:46 +0100

    mise en place html et css

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,331 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import {
+    RouterLink,
+    RouterView
+  } from 'vue-router'
+  import Test from './components/Accueil.vue'
+
+
+function swip(){
+  const content = document.querySelector('#content1')
+  content.style.transform= ' translateY(-2000px)'
+  document.querySelector('#smoke').style.opacity="0"
+  
+}
+
+
 </script>
 
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="content1">
+    <div id="header">
+      <p>Pitois Guillaume</p>
+      <button @click="swip()" id="entrer">Entrer</button>
     </div>
-  </header>
 
-  <RouterView />
+    <figure>
+      <img src="https://picsum.photos/200/300" alt="Photo du propriétaire du site ">
+      <figcaption >Developper Web <br>Junior</figcaption>
+      <div id="js"></div>
+    </figure>
+    
+  </div>
+
+
+  <div id="smoke"></div>
+  <div id="content2">
+    <header>
+      <img alt="Visage de la personne" class="logo" src="https://picsum.photos/200/300" width="240" height="240" />
+
+      <div id="moi">
+
+        <div id="identite">
+
+
+          <ul>
+            <li>
+              <p>Pitois</p>
+            </li>
+            <li>
+              <p>Guillaume</p>
+            </li>
+            <li>
+              <p>21 ans</p>
+            </li>
+          </ul>
+
+        </div>
+
+
+
+        <div id="profession">
+          <p>Developper junior</p>
+          <div>Javascript </div>
+        </div>
+
+      </div>
+
+
+
+      <!-- <nav>
+        <RouterLink to="/">accueil</RouterLink>
+       
+      </nav> -->
+
+    </header>
+
+    <!-- 
+  <RouterView /> -->
+    <main>
+     
+      <!-- <div id="js"></div> -->
+      <div id="menu">menu</div>
+    </main>
+
+
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+*{
+  /* font-family: 'Source Code Pro', monospace; */
+  font-family: 'Caveat', cursive;
+  
+  font-weight: 900
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@800&display=swap');
 
-nav {
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@700&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Titan+One&display=swap');
+
+
+
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,900;1,800&display=swap');
+
+
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap');
+
+#smoke{
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100vh;
+  position: absolute;
+  opacity: 1;
+  transition: 0.8s linear;
+  background-color: #000000;
+  z-index: 2;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#content1{
+  background-color: #f2f2f2;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  z-index: 3;
+  display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ 
+ transition: 1s linear;
+ 
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#header{
+  width: 100%;
+  color: #111;
+ 
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  
+  font-size: 1.4rem;
+  font-weight: 800;
+  text-shadow: 1px 1px 2px #00000080;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+#entrer{
+  color: #333;
+  padding: 8px 16px;
+  border-radius: 3px;
+  letter-spacing: 0.6px;
+  border: none;
+ 
+
+    border-radius: 54px;
+background: linear-gradient(145deg, #dadada, #ffffff);
+box-shadow:  7px 7px 8px #b6b6b6,
+             -7px -7px 8px #ffffff;
+
 }
 
-nav a:first-of-type {
-  border: 0;
+#content1 figure{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
 }
 
-@media (min-width: 1024px) {
+#content1 figure img{
+  position: relative;
+  box-shadow: 0px 0px 20px 0px #00000024;
+  height: 540px;
+  width: 80%;
+
+  border-radius: 15px;
+background: linear-gradient(145deg, #ffffff, #dadada);
+box-shadow:  35px 35px 70px #616161,
+             -30px -30px 70px #ffffff;
+  
+}
+
+#content1 figure figcaption{
+  position: absolute;
+  font-size: 2.2rem;
+  color: #fff;
+  text-shadow: 0px 0px 8px black;
+  
+}
+
+
+  #content2 {
+    display: grid;
+    width: 100%;
+    /* display: none; */
+    height: 100vh;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      'header'
+      'main';
+    box-shadow: 0px 0px 20px 0px #000000;
+    color: #3e603d;
+
+  
+  
+
+  }
+
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: end;
+    align-items: center;
+   
+    width: 100%;
+   
+    
+    font-size: 1.1rem;
+    grid-area: 'header';
+    position: relative;
+    min-height: 170px;
+    color: #000;
+    background: linear-gradient(145deg, #dadada, #ffffff);
+box-shadow:  7px 7px 8px #b6b6b6,
+             -7px -7px 8px #ffffff;
+  }
+
+  #identite {
+
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+
+  header #identite ul {
+
+    list-style: none;
+    
+    
+  }
+
+  #moi {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 100%;
+    width: 50%;
+    
+
+  }
+
+  #profession {
+
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    border-radius: 50%;
+    position: absolute;
+    z-index: 1;
+    left: -36px;
+    top: -36px;
+    background: linear-gradient(145deg, #dadada, #ffffff);
+box-shadow:  7px 7px 8px #b6b6b6,
+             -7px -7px 8px #ffffff;
+
   }
 
-  header .wrapper {
+
+  main {
+    position: relative;
+    grid-area: 'main';
+    background-color: #f2f2f2;
+    box-shadow: inset 0px 0px 20px 2px #b7b7b7;
+
+  }
+
+  #js {
+    width: 100px;
+    height: 100px;
+    background: url('./assets/img/js-logo.png') center/cover no-repeat;
+    border-radius: 20px;
+    position: absolute;
+    margin-top: 110px;
+    margin-left: 100px;
+    transform: translateX(-1200px);
+    animation: js 1s linear forwards;
+    
+    
+
+  }
+
+ 
+
+  @keyframes js{
+    from{
+      transform: translateX(-1200px) rotate(0deg);
+
+    }
+    to{
+      transform: translateX(0) rotate(360deg);
+    }
+  }
+
+  #menu {
+    height: 50px;
+    width: 50px;
+    background: linear-gradient(145deg, #dadada, #ffffff);
+box-shadow:  7px 7px 8px #b6b6b6,
+             -7px -7px 8px #ffffff;
+    border-radius: 50%;
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/assets/app.js
@@ -0,0 +1,17 @@
+
+
+
+// function swip(){
+//     const content =  document.querySelector('#content1')
+// content.style.transform=  'translateY(-2000px)'
+// }
+
+let test = document.querySelector('#entrer')
+
+test.addEventListener('click',swip())
+
+function swip(){
+    console.log('coucou');
+}
+
+console.log(test);
